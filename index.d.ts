6eb95286ff66fba697b0a3e762f5f27c807ea143
Jinkyuhan <gkswlsrb95@gmail.com>
2022-02-04 16:25:30 +0900

    test: fastify.container type included

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,16 @@
 /// <reference types="node" />
 import type { FastifyPluginCallback } from 'fastify/types/plugin';
-export declare type ComponentRegistry = {
-  [key: string]: unknown;
-};
-export declare type ComponentFactory = {
-  [key: string]: (...args: any[]) => object;
-};
 export interface ComponentSummary {
-  name: string;
-  constructor: (...args: any[]) => any;
+    name: string;
+    constructor: (...args: any[]) => any;
 }
 export interface Container {
-  get: <T extends unknown>(name: string) => T;
+    get: <T extends unknown>(name: string) => T;
 }
 export interface ContainerPluginOptions {
-  components: ComponentSummary[];
-  containerName?: string;
-  onInitialized?: <T extends unknown>(
-    componentName: string,
-    initializedComponent: T
-  ) => Promise<void> | void;
+    components: ComponentSummary[];
+    containerName?: string;
+    onInitialized?: <T extends unknown>(componentName: string, initializedComponent: T) => Promise<void> | void;
 }
-declare const _default: FastifyPluginCallback<
-  ContainerPluginOptions,
-  import('http').Server
->;
+declare const _default: FastifyPluginCallback<ContainerPluginOptions, import("http").Server>;
 export default _default;
